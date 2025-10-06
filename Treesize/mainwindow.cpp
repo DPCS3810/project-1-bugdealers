47affc44e3d21487cab2999c2ed1731736e00948
Poulomi-Sarkar-7 <poulomivms@gmail.com>
2025-10-06 19:13:49 +0530

    Basic File structure added

--- Treesize/mainwindow.cpp
+++ Treesize/mainwindow.cpp
@@ -1,14 +1,83 @@
 #include "mainwindow.h"
-#include "./ui_mainwindow.h"
+#include "ui_mainwindow.h"
+#include <QDir>
+#include <QFileInfo>
+#include <QTreeWidgetItem>
+#include <QDebug>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    // Setup TreeWidget
+    ui->treeWidget->setColumnCount(3);
+    QStringList headers = {"Name", "Size (bytes)", "File Count"};
+    ui->treeWidget->setHeaderLabels(headers);
+
+    // Scan a folder (change this to any folder path)
+    QString rootPath = QDir::homePath();
+    FileNode rootNode;
+    rootNode.name = rootPath;
+    rootNode.path = rootPath;
+    rootNode.isFolder = true;
+
+    scanDirectory(rootPath, rootNode);
+
+    // Populate TreeWidget
+    QTreeWidgetItem *rootItem = new QTreeWidgetItem(ui->treeWidget);
+    rootItem->setText(0, rootNode.name);
+    rootItem->setText(1, QString::number(rootNode.size));
+    rootItem->setText(2, QString::number(rootNode.fileCount));
+
+    populateTree(rootNode, rootItem);
+
+    ui->treeWidget->addTopLevelItem(rootItem);
+    ui->treeWidget->expandAll();
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+// Recursive DFS scan
+void MainWindow::scanDirectory(const QString &path, FileNode &node)
+{
+    QDir dir(path);
+    QFileInfoList entries = dir.entryInfoList(QDir::NoDotAndDotDot | QDir::AllEntries);
+
+    for (QFileInfo entry : entries) {
+        FileNode child;
+        child.name = entry.fileName();
+        child.path = entry.filePath();
+        child.isFolder = entry.isDir();
+        child.size = entry.isFile() ? entry.size() : 0;
+
+        if (child.isFolder) {
+            scanDirectory(child.path, child);
+            node.size += child.size;
+            node.fileCount += child.fileCount;
+        } else {
+            node.size += child.size;
+            node.fileCount += 1;
+        }
+
+        node.children.append(child);
+    }
+}
+
+// Populate QTreeWidget recursively
+void MainWindow::populateTree(const FileNode &node, QTreeWidgetItem *parentItem)
+{
+    for (const FileNode &child : node.children) {
+        QTreeWidgetItem *item = new QTreeWidgetItem(parentItem);
+        item->setText(0, child.name);
+        item->setText(1, QString::number(child.size));
+        item->setText(2, QString::number(child.fileCount));
+
+        if (child.isFolder)
+            populateTree(child, item);
+    }
 }
--- Treesize/mainwindow.h
+++ Treesize/mainwindow.h
@@ -2,11 +2,19 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QTreeWidget>
+
+struct FileNode {
+    QString name;
+    QString path;
+    quint64 size = 0;
+    bool isFolder = false;
+    quint64 fileCount = 0;
+    QList<FileNode> children;
+};
 
 QT_BEGIN_NAMESPACE
-namespace Ui {
-class MainWindow;
-}
+namespace Ui { class MainWindow; }
 QT_END_NAMESPACE
 
 class MainWindow : public QMainWindow
@@ -19,5 +27,8 @@ public:
 
 private:
     Ui::MainWindow *ui;
+
+    void scanDirectory(const QString &path, FileNode &node);
+    void populateTree(const FileNode &node, QTreeWidgetItem *parentItem);
 };
 #endif // MAINWINDOW_H
